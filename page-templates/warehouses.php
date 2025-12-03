98c88f4b191af496f2da2a2207dc1d81ece4200c
Troy Cono <troy.cono@quility.com>
2025-12-03 18:40:36 -0500

    Fixes styling and removes duplicate warehouse FacetWP template calls on the warehouses page

--- page-templates/warehouses.php
+++ page-templates/warehouses.php
@@ -9,24 +9,6 @@ get_header(); ?>
 	<?php while(have_posts()) { ?>
 		<?php the_post(); ?>
 		<?php get_template_part('blocks/flexible-content'); ?>
-        
-        <section class="warehouse-finder">
-             <div class="warehouse-proximity mobile">
-                <?php echo do_shortcode('[facetwp facet="proximity"]'); ?>
-            </div>
-            <div class="warehouse-map">
-                <?php echo do_shortcode('[facetwp facet="warehouse_map"]'); ?>
-            </div>
-            <div class="map-filters">
-                <div class="warehouse-proximity desktop">
-                    <?php echo do_shortcode('[facetwp facet="proximity"]'); ?>
-                </div>
-                <div class="warehouse-listings">
-                    <?php echo do_shortcode('[facetwp template="warehouses"]'); ?>
-                </div>
-            </div>
-        </section>
-        
 	<?php } ?>
 </main><!-- #main -->
 
